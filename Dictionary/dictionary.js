fef81404f0944c37aa18d9fb354b2de6c621d09a
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-03-01 22:18:01 +0530

    Update dictionary.js

--- Dictionary/dictionary.js
+++ Dictionary/dictionary.js
@@ -22,7 +22,7 @@ search_btn.addEventListener( 'click', ()=>{
         <div class="row d-flex flex-row mb-4">
         <div class="col-6 pb-0 ">
             <p class="text-start fs-6 -dark mb-0">
-            ${data_received[0].meanings[0].partOfSpeech}, ${data_received[0].phonetic}
+            ${data_received[0].meanings[0].partOfSpeech} , ${data_received[0].phonetic}
             </p>
             <p class="text-start fs-6 mt-0">Antonyms: ${data_received[0].meanings[0].antonyms} </p>
         </div>
@@ -30,17 +30,22 @@ search_btn.addEventListener( 'click', ()=>{
         
         <div class="row mb-2" id="content-bar">
         <div class="col-12">
-            <p class="ms-2 ps-3 pb-2 text-start text-wrap fs-5 lh-base fw-lighter text-break">
+            <p class="ms-2 ps-3 text-start text-wrap fs-5 lh-base fw-lighter text-break">
                 1. ${data_received[0].meanings[0].definitions[0].definition}<br>    
                 2. ${data_received[0].meanings[1].definitions[0].definition}    
             </p>
         </div>`;
-        audio.setAttribute("src", data_received[0].phonetics[0].audio)
+        audio.setAttribute("src", 
+        data_received[0].phonetics[0].audio || data_received[0].phonetics[1].audio ||
+        data_received[0].phonetics[2].audio || data_received[0].phonetics[3].audio)
+    })
+    .catch((error) =>{
+        
     })
 });
 
 function playvoice(){
    let audio_icon=document.getElementById('audio-icon');
-   audio_icon.innerHTML='<i class="bi bi-volume-up-fill" onclick="playvoice()"></i>';
-   audio.play();    
+    audio_icon.innerHTML='<i class="bi bi-volume-up-fill" onclick="playvoice()"></i>';
+    audio.play();
 }
